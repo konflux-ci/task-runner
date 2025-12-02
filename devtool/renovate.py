55176b4a5d791886d84652f312df2f9ed5345f3c
Adam Cmiel <acmiel@redhat.com>
2025-12-02 10:51:20 +0100

    renovate: run at any time (temporarily)
    For testing the configuration
    
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- devtool/renovate.py
+++ devtool/renovate.py
@@ -7,8 +7,7 @@ def renovate_json(go_packages: list[GoPackage]) -> dict[str, Any]:
     return {
         "$schema": "https://docs.renovatebot.com/renovate-schema.json",
         "extends": ["config:recommended"],
-        # Run bi-weekly on the 1st and 15th of each month
-        "schedule": ["* * 1,15 * *"],
+        "schedule": ["* * * * *"],
         "prHourlyLimit": 0,  # unlimited
         "git-submodules": {
             "enabled": True,
